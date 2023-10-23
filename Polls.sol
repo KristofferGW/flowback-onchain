c981bcf74345a55cb7251e44dd1a576ea9a11fe0
Kristoffer Wallqvist <112932893+KristofferGW@users.noreply.github.com>
2023-10-23 18:53:05 +0200

    Created Poll struct

--- Polls.sol
+++ Polls.sol
@@ -1,7 +1,15 @@
 pragma solidity ^0.8.0;
 
 contract Polls {
-    string name;
-    string tag;
+    struct Poll {
+        string title;
+        string tag;
+        uint pollStartDate;
+        uint proposalEndDate;
+        uint votingStartDate;
+        uint delegateEndDate;
+        uint endDate;
+        uint pollId;
+    }
     
 }
