74745fed185dff235014698bb9c073944c96ea5e
Ellen <_ellen@live.se>
2023-11-21 15:21:58 +0100

    testingDraft

--- script/Prediction.t.sol
+++ script/Prediction.t.sol
@@ -3,16 +3,33 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "src/Prediction.sol";
+import "forge-std/Vm.sol";
+import "forge-std/console.sol";
 
 contract PredictionTest is Test, Predictions{
-    function setUp() public {}
+
+    Predictions public prediction;
+    // vm.startPrank(0x18d1161FaBAC4891f597386f0c9B932E3fD3A1FD);
+
+    function setUp() public {
+        prediction = new Predictions();
+    }
 
     function run() public {
         vm.broadcast();
     }
 
-    function testCreatePrediction() public {
+    // function testCreatePrediction() public {
         
-    }
+    //     uint _proposalId = 1;
+    //     string memory _description = "pred";
+    //     uint _likelihood= 4;
+    //     uint _pollId = 1;
+     
+    //     // bool success = prediction.createPrediction(_proposalId, _description, _likelihood, _pollId);
+       
+    //     // console.log(success);
+
+    // }
     
 }
--- script/RightToVote.t.sol
+++ script/RightToVote.t.sol
@@ -3,24 +3,28 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "src/RightToVote.sol";
+import "forge-std/Vm.sol";
 
 contract RightToVoteTest is Test, RightToVote {
 
     RightToVote public rightToVote;
-
+    //vm.startPrank(0x18d1161FaBAC4891f597386f0c9B932E3fD3A1FD);
+    
     function setUp() public {
         rightToVote = new RightToVote();
     }
 
     function run() public {
-        vm.broadcast();
+       vm.broadcast(); 
     }
 
-    function testGiveRightToVote(uint num) public {
-        
-        rightToVote.giveRightToVote(num);
-        assertEq(rightToVote.checkRightsInGroup(num), true);
+    function testGiveRightToVote(uint group) public {
+        rightToVote.giveRightToVote(group);
+        assertTrue(rightToVote.checkRightsInGroup(group));
+    }
 
+    function testRemoveRightToVote(uint group) public {
+        assertEq(rightToVote.checkRightsInGroup(group), false);
     }
     
 }
