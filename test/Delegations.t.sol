d45ecfd11ad37550661146ab29cfff9ddbdcdf88
Ellen <_ellen@live.se>
2023-11-29 18:39:34 +0100

    aa

--- test/Delegations.t.sol
+++ test/Delegations.t.sol
@@ -1,1 +1,18 @@
-// This file will include the tests related to the Delegations contract
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "src/Delegations.sol";
+
+contract DelegationsTest is Test, Delegations {
+    function setUp() public {}
+
+    function run() public {
+        vm.broadcast();
+    }
+
+    function testBecomeDelegate() public {
+        
+    }
+    
+}
