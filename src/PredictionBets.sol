a5df4c77a5678e8dce2b517483eeeec2fa4f4418
EllenLng <_ellen@live.se>
2024-04-12 12:54:32 +0200

    optimized predictionBets

--- src/PredictionBets.sol
+++ src/PredictionBets.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
+
 import {PollHelpers} from './PollHelpers.sol';
 import {ProposalHelpers} from './ProposalHelpers.sol';
 import {PredictionHelpers} from './PredictionHelpers.sol';
@@ -9,94 +10,53 @@ contract PredictionBets is PollHelpers, ProposalHelpers, PredictionHelpers, Pred
 
     event PredictionBetCreated(uint indexed predictionId, bool bet, uint likelihood);
 
-    function _requireExist(uint _pollId, uint _proposalId, uint _predictionId) private view {
-        require(requirePollPropPredToExist(_pollId, _proposalId, _predictionId), "Wrong poll, proposal or prediction");
+    function requirePollPropPredToExist(uint _pollId, uint _proposalId, uint _predictionId) internal view returns (bool) {
+        uint pollsLength = pollCount;
+        for (uint i = 0; i < pollsLength; i++) {
+            if (polls[i].pollId == _pollId) {
+                uint proposalsLength = proposals[_pollId].length;
+                for (uint j = 0; j < proposalsLength; j++) {
+                    if (proposals[_pollId][j].proposalId == _proposalId) {
+                        uint predictionsLength = predictions[_proposalId].length;
+                        for (uint k = 0; k < predictionsLength; k++) {
+                            if (predictions[_proposalId][k].predictionId == _predictionId) {
+                                return true;
+                            }
+                        }
+                        return false;
+                    }
+                }
+                return false;
+            }
+        }
+        return false;
     }
 
-    modifier requireExist(uint _pollId, uint _proposalId,uint _predictionId){
-        _requireExist(_pollId, _proposalId, _predictionId);
+    modifier requireExist(uint _pollId, uint _proposalId, uint _predictionId) {
+        require(requirePollPropPredToExist(_pollId, _proposalId, _predictionId), "Wrong poll, proposal or prediction");
         _;
     }
+
     function placePredictionBet(
         uint _pollId,
         uint _proposalId,
-        uint _predictionId, 
+        uint _predictionId,
         uint _likelihood,
         bool _bet
-    )  external requireExist(_pollId, _proposalId, _predictionId){
-      
-        require(_likelihood > 0 , "Value needs to be higher than 0");
-          
-        predictionBets[_predictionId].push(PredictionBet({
+    ) external requireExist(_pollId, _proposalId, _predictionId) {
+        require(_likelihood > 0, "Likelihood must be greater than 0");
+        PredictionBet[] storage bets = predictionBets[_predictionId];
+        bets.push(PredictionBet({
             pollId: _pollId,
             proposalId: _proposalId,
             predictionId: _predictionId,
             likelihood: _likelihood,
             bet: _bet
         }));
-            
         emit PredictionBetCreated(_predictionId, _bet, _likelihood);
     }
 
-    function requirePollPropPredToExist(uint _pollId, uint _proposalId, uint _predictionId) internal view returns (bool exists){
-
-        //require poll to exist
-        uint pollsLength = pollCount;
-        for (uint a; a <= pollsLength;){
-            if(polls[a].pollId==_pollId) {
-                //require proposal to exist
-                uint proposalsLength= proposals[_pollId].length;
-                // if(proposalsLength==0)return false;
-                for (uint b; b < proposalsLength;){
-                    if (proposals[_pollId][b].proposalId==_proposalId) {
-                        //require prediction to exist
-                        uint predictionsLength = predictions[_proposalId].length;
-                        // if(predictionsLength==0)return false;
-                        for (uint c; c < predictionsLength;){
-                            if (predictions[_proposalId][c].predictionId ==_predictionId){
-                                return true; 
-                            }
-                            unchecked {
-                                ++c;
-                            }
-                        }
-                        return false;
-                    }
-                    unchecked {
-                        ++b;
-                    }
-                }
-                return false;
-            }
-            unchecked {
-                ++a;
-            } 
-        }
-        return false;
-    }
-
-    function getPredictionBets( 
-        uint _pollId, 
-        uint _proposalId, 
-        uint _predictionId
-        ) external requireExist(_pollId, _proposalId, _predictionId) view returns(PredictionBet[] memory) {
-
-        uint proposalsLength = proposals[_pollId].length;
-        for (uint a=0; a <= proposalsLength;){
-            if(proposals[_pollId][a].proposalId == _proposalId){
-                uint predictionsLength =predictions[_proposalId].length;
-                for (uint b=0; b <= predictionsLength;){
-                    if(predictions[_proposalId][b].predictionId == _predictionId)
-                        return predictionBets[_predictionId];
-                    unchecked {
-                        ++b;
-                    }
-                }
-            }
-            unchecked {
-                ++a;
-            }
-        }
+    function getPredictionBets(uint _pollId, uint _proposalId, uint _predictionId) external requireExist(_pollId, _proposalId, _predictionId) view returns (PredictionBet[] memory) {
         return predictionBets[_predictionId];
     }
 }
